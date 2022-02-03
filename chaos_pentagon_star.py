c7682b2569b7a16802af41d0738cede944b26992
Davin Dillon <30303781+DavinDillon@users.noreply.github.com>
2022-02-03 13:33:07 -0600

    Update chaos_pentagon_star.py
    added more comments to make it a bit clearer

--- chaos_pentagon_star.py
+++ chaos_pentagon_star.py
@@ -4,17 +4,17 @@ import random
 import math
 
 
-A = (-242.71,85.32)
+A = (-242.71,85.32)  # points for the vertices of a pentagon
 B = (0.00,261.65)
 C = (242.71,85.32)
 D = (150.00,-200.00)
 E = (-150.00,-200.00)
 colors = ['red', 'orange', 'yellow', 'green', 'blue','indigo', 'purple']
 t = turtle.Turtle()
-t.speed(0)
+#t.speed(0)
 t.hideturtle()
-scn = turtle.Screen()
-scn.tracer(False)
+scn = turtle.Screen()   
+scn.tracer(False)            # make it run really fast
 scn.bgcolor('black')
 t.pencolor('white')
 t.penup()
@@ -28,10 +28,10 @@ for i in range(25000):
 
     t.pencolor(colors[y])
     if temp == temp2:
-        if abs(x-temp) != 1 :
-            if abs(x-temp)!= 4:
+        if abs(x-temp) != 1 :  # skip if the vertex neighbors the last chosen vertex
+            if abs(x-temp)!= 4   # the one off case where abs 1 doesn't catch neighbor(1-5 or 5-1)
                     
-                if x == 1:
+                if x == 1:            # choose direction based on vertex
                     dist = t.distance(A)
                     t.setheading(t.towards(A))
                 elif x ==2:
@@ -46,15 +46,15 @@ for i in range(25000):
                 elif x == 5:
                     dist = t.distance(E)
                     t.setheading(t.towards(E))
-                t.forward(dist/2)
+                t.forward(dist/2)           # jump half the distance to the vertex
                 t.pendown()
-                t.dot(3)
+                t.dot(3)            # place a dot
                 t.penup()
                 scn.update()
-                temp2 = temp
-                temp = x
+                temp2 = temp        # set temp2 for next time
+                temp = x         # reset temp to check if the previous 2 vertices are the same
 
-    else:
+    else:                   # if the last two vertices were not the same just go halfway to chosen vertex
         if x == 1:
             dist = t.distance(A)
             t.setheading(t.towards(A))
@@ -82,5 +82,5 @@ for i in range(25000):
         print('done')   
         
     
-scn.tracer(True)
-scn.mainloop()
+scn.tracer(True)   # reset scn.Tracer to default to avoid future issues
+scn.mainloop()      # close out mainloop
