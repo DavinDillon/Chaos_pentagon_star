71407ca396ed9734de34444a43a4a05f67504b92
Davin Dillon <30303781+DavinDillon@users.noreply.github.com>
2022-02-03 13:20:56 -0600

    Add files via upload
    Chaos star python

---
+++ chaos_pentagon_star.py
@@ -0,0 +1,86 @@
+
+import turtle
+import random
+import math
+
+
+A = (-242.71,85.32)
+B = (0.00,261.65)
+C = (242.71,85.32)
+D = (150.00,-200.00)
+E = (-150.00,-200.00)
+colors = ['red', 'orange', 'yellow', 'green', 'blue','indigo', 'purple']
+t = turtle.Turtle()
+t.speed(0)
+t.hideturtle()
+scn = turtle.Screen()
+scn.tracer(False)
+scn.bgcolor('black')
+t.pencolor('white')
+t.penup()
+t.goto(-80,80)
+temp = 0
+turtle.title('Chaos game pentagon star')
+temp2 = 0
+for i in range(25000):
+    x = random.randint(1,5)
+    y = random.randint(0,6)
+
+    t.pencolor(colors[y])
+    if temp == temp2:
+        if abs(x-temp) != 1 :
+            if abs(x-temp)!= 4:
+                    
+                if x == 1:
+                    dist = t.distance(A)
+                    t.setheading(t.towards(A))
+                elif x ==2:
+                    dist = t.distance(B)
+                    t.setheading(t.towards(B))
+                elif x == 3:
+                    dist = t.distance(C)
+                    t.setheading(t.towards(C))
+                elif x == 4:
+                    dist = t.distance(D)
+                    t.setheading(t.towards(D))
+                elif x == 5:
+                    dist = t.distance(E)
+                    t.setheading(t.towards(E))
+                t.forward(dist/2)
+                t.pendown()
+                t.dot(3)
+                t.penup()
+                scn.update()
+                temp2 = temp
+                temp = x
+
+    else:
+        if x == 1:
+            dist = t.distance(A)
+            t.setheading(t.towards(A))
+        elif x ==2:
+            dist = t.distance(B)
+            t.setheading(t.towards(B))
+        elif x == 3:
+            dist = t.distance(C)
+            t.setheading(t.towards(C))
+        elif x == 4:
+            dist = t.distance(D)
+            t.setheading(t.towards(D))
+        elif x == 5:
+            dist = t.distance(E)
+            t.setheading(t.towards(E))
+        t.forward(dist/2)
+        t.pendown()
+        t.dot(3)
+        t.penup()
+        scn.update()
+
+        temp2 = temp
+        temp = x
+    if i == 24999:
+        print('done')   
+        
+    
+scn.tracer(True)
+scn.mainloop()
